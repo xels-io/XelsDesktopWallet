103afbc1f75a3d640c0fcb5d4d95f8a24b4fff3d
Mahfuz Rahman <developerpro2011@gmail.com>
2020-11-11 18:11:39 +0600

    exchange rate calculation real time exchange received amount view current token wallet retrieve

--- src/app/exchange/exchange/exchange.component.ts
+++ src/app/exchange/exchange/exchange.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { GlobalService } from '@shared/services/global.service';
+import { TokenService } from '@shared/services/token.service';
 
 @Component({
   selector: 'app-exchange',
@@ -7,6 +9,7 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./exchange.component.css']
 })
 export class ExchangeComponent implements OnInit {
+  received_amount = 0;
   exchangeForm = new FormGroup({
     from: new FormControl('',[
       Validators.required
@@ -15,13 +18,26 @@ export class ExchangeComponent implements OnInit {
       Validators.required
     ])
   })
-  constructor() { }
+  constructor(
+    private globalService : GlobalService,
+    private Token : TokenService
+  ) { }
 
   ngOnInit() {
   }
 
   exchangeNow(){
     console.log(this.exchangeForm.value)
+    let walletName = this.globalService.getWalletName();
+    let tokenWallet = this.Token.getLocalWallet(walletName,this.exchangeForm.value.from);
+    console.log(walletName,tokenWallet);
+  }
+  updateReceivedAmount(){
+    if(this.exchangeForm.value.deposit_amount>0){
+      this.received_amount = this.exchangeForm.value.deposit_amount*0.1;
+    }else{
+      this.received_amount = 0;
+    }
   }
 
 }
